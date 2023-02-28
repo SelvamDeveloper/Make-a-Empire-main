813814b7441b8dc4a5c813eb1e6cb6654b0e3207
Simply one <selvam.s3403@gmail.com>
2023-02-28 15:13:51 +0530

    sad

--- main.js
+++ main.js
@@ -3,11 +3,19 @@ let c2 = document.getElementById("chapter02");
 let c3 = document.getElementById("chapter03");
 let c4 = document.getElementById("chapter04");
 let c5 = document.getElementById("chapter05");
+let c6 = document.getElementById("chapter06");
+let c7 = document.getElementById("chapter07");
+let c8 = document.getElementById("chapter08");
+let c9 = document.getElementById("chapter09");
 let cBox01 = document.querySelector(".chapter-1");
 let cBox02 = document.querySelector(".chapter-2");
 let cBox03 = document.querySelector(".chapter-3");
 let cBox04 = document.querySelector(".chapter-4");
 let cBox05 = document.querySelector(".chapter-5");
+let cBox06 = document.querySelector(".chapter-6");
+let cBox07 = document.querySelector(".chapter-7");
+let cBox08 = document.querySelector(".chapter-8");
+let cBox09 = document.querySelector(".chapter-9");
 
 
 // box design 
@@ -27,7 +35,18 @@ c4.addEventListener("click", ()=>{
 c5.addEventListener("click", ()=>{
     cBox05.style.display="flex";
 });
-
+c6.addEventListener("click", ()=>{
+    cBox06.style.display="flex";
+});
+c7.addEventListener("click", ()=>{
+    cBox07.style.display="flex";
+});
+c8.addEventListener("click", ()=>{
+    cBox08.style.display="flex";
+});
+c9.addEventListener("click", ()=>{
+    cBox09.style.display="flex";
+});
 
 // back button js 
 
@@ -37,4 +56,8 @@ function goBack(){
     cBox03.style.display="none";
     cBox04.style.display="none";
     cBox05.style.display="none";
+    cBox06.style.display="none";
+    cBox07.style.display="none";
+    cBox08.style.display="none";
+    cBox09.style.display="none";
 }
